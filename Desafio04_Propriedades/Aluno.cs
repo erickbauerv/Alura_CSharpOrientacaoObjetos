efb913eba56226a882bb80132ca774ddf0845a94
Erick Bauer <ebauer@gvdasa.com.br>
2022-12-21 17:20:13 -0300

    Desafio04 - Praticando criar validação ao definir um valor para uma propriedade.

---
+++ Desafio04_Propriedades/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio04_Propriedades
+{
+    class Aluno
+    {
+        public int _matricula;
+        private int Matricula
+        {
+            get { return _matricula}
+            set
+            {
+                if (value > 0)
+                {
+                    _matricula = value;
+                }
+                else
+                {
+                    Console.WriteLine("O valor atribuido não pode ser menor que 1");
+                }
+            }
+        }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+        public int TipoCurso { get; set; }
+    }
+}
---
+++ Desafio04_Propriedades/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio04_Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
